007ac95e81c76e14ae73d0f57ea32374cbce070a
Gabriel Gonzales <gabrielgonzales0007@gmail.com>
2025-04-09 01:27:53 -0300

    Incluindo função do carrossel

--- js/script.js
+++ js/script.js
@@ -26,3 +26,63 @@ window.onload = () => {
     alert("Cadastro realizado com sucesso!");
     fecharPopup();
   }
+
+  function abrirPopupNovamente() {
+    const popup = document.getElementById('popup');
+    const formulario = document.getElementById('formulario');
+  
+    popup.style.display = 'flex';
+    formulario.style.display = 'none'; // volta pra tela inicial do modal
+  }
+  
+
+  const track = document.querySelector('.carousel-track');
+  const images = Array.from(track.children);
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  
+  let index = 0;
+  let interval;
+  
+  // Deixa imagem central focada
+  function updateCarousel() {
+    images.forEach((img, i) => {
+      img.classList.toggle('active', i === index);
+    });
+  
+    const imageWidth = images[0].getBoundingClientRect().width + 20;
+    const trackWidth = imageWidth * images.length;
+    const translateX = -(imageWidth * index - (window.innerWidth / 2 - imageWidth / 2));
+  
+    track.style.transform = `translateX(${translateX}px)`;
+  }
+  
+  // Mover para próxima imagem
+  function nextImage() {
+    index = (index + 1) % images.length;
+    updateCarousel();
+  }
+  
+  // Mover para imagem anterior
+  function prevImage() {
+    index = (index - 1 + images.length) % images.length;
+    updateCarousel();
+  }
+  
+  
+  // Navegação por botões
+  nextButton.addEventListener('click', () => {
+    nextImage();
+  });
+  
+  prevButton.addEventListener('click', () => {
+    prevImage();
+  });
+  
+  // Resize responsivo
+  window.addEventListener('resize', updateCarousel);
+  
+  // Iniciar
+  updateCarousel();
+  
+  
